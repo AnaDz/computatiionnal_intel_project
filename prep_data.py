46dd37e5b83e305754566220d5594234bf4cf323
anaDz <anais.donzeau@hotmail.fr>
2017-11-29 22:59:53 -0500

    process donnees : 1ere table créee

--- prep_data.py
+++ prep_data.py
@@ -17,45 +17,70 @@ if __name__ == '__main__':
         data = data.dropna()
         data = data.reset_index()
         data = data.drop(columns=['index'])
+
+        first_date = datetime.datetime.strptime(data.loc[0,'observationDate'], "%d/%m/%Y")
+        data_process = pd.DataFrame({'region':data.loc[0,'region'],
+                                     'observationDateYear': first_date.year,
+                                     'observationDateMonth': first_date.month,
+                                     'disease': data.loc[0,'disease'],
+                                     'sumCases': data.loc[0,'sumCases']},index=[0])
         
-        print data.head()
-        for i in range(len(data.observationDate)):
-            date = data.loc[i,'observationDate']
-            if (isinstance(date, str)):
-                   data.loc[i,'observationDate'] = datetime.datetime.strptime(date, "%d/%m/%Y")
-        print(data.observationDate[1])
-        data.to_csv('good_data.csv')
+#        print data.head()
+        for i in range(1,len(data.observationDate)):
+            date = datetime.datetime.strptime(data.loc[i,'observationDate'], "%d/%m/%Y")
+            data_process = data_process.append(pd.DataFrame({'region':data.loc[i,'region'],
+                                     'observationDateYear': date.year,
+                                     'observationDateMonth': date.month,
+                                     'disease': data.loc[i,'disease'],
+                                              'sumCases': data.loc[i,'sumCases']},index=[i]), ignore_index = True)
+            #if (isinstance(date, str)):
+                   #data.loc[i,'observationDate'] = datetime.datetime.strptime(date, "%d/%m/%Y")
+       # print(data.observationDate[1])
+        data_process.to_csv('good_data.csv')
+
+        #processing data : creation of a new panda table with month and year instead of one date
+        
+        print data_process
+
     else:
-         data = pd.read_csv('good_data.csv')
-         data[['observationDate']] = data[['observationDate']].apply(pd.to_datetime)
+         data_process = pd.read_csv('good_data.csv')
 
     #most_common_diseases = data.disease.index
-    all_diseases = data.disease.value_counts().index
+    all_diseases = data_process.disease.value_counts().index
 
-    all_countries = data.region.value_counts().index
-    
-    for disease in all_diseases[0:1]:
-        print(disease)
-        one_disease_table = data.loc[data['disease'] == disease]
-        #for i in one_disease_table:
-         #   print type(i)
-        for country in all_countries[0:1]:
-            print country
+    all_countries = data_process.region.value_counts().index
+
+    final_data =  pd.DataFrame({'region': np.nan, 'observationDateYear': np.nan, 'observationDateMonth': np.nan, 'disease': np.nan, 'sumCases': np.nan}, index = [0])
+    print final_data
+        
+    for disease in all_diseases:
+        one_disease_table = data_process.loc[data_process['disease'] == disease]
+        for country in all_countries:
             one_country_table = one_disease_table.loc[one_disease_table['region'] == country]
             if len(one_country_table)!=0:
-                date_min = min(one_country_table.observationDate)
-                date_max = max(one_country_table.observationDate)
-                for year in range(date_min.year,date_max.year+1):
-                    for month in range(date_min.month,date_max.month+1):
-                        number_cases = 0
+                year_min = min(one_country_table.observationDateYear)
+                month_min = min(one_country_table.loc[one_country_table['observationDateYear']==year_min].observationDateMonth)
+                year_max = max(one_country_table.observationDateYear)
+                month_max = max(one_country_table.loc[one_country_table['observationDateYear']==year_max].observationDateMonth)
+                for year in range(year_min,year_max+1):
+                    for month in range(month_min,month_max+1):
                         if (len(one_country_table)!=0):
-                            print one_country_table.observationDate.head(1)
-                            print datetime.datetime(year, month%12 +1 , 1)-datetime.timedelta(days=1)
-                            delta = (one_country_table.observationDate.head(1) - datetime.datetime(year, (month-1)%12 +1 , 1)-datetime.timedelta(days=1))
-                            print delta
-                        case_in_month = one_country_table.loc[(one_country_table['observationDate'] - datetime.datetime(year, (month-1)%12 +1 , 1)-datetime.timedelta(days=1)).days <= 31]
-                       
-                        if len(case_in_month) != 0:
-                            number_cases = case_in_month.sumCases.sum()
-                            #print number_cases
-            #processed_data = 0 # les datas une fois bien rangee et tout
+                            case_in_month = one_country_table.loc[(one_country_table['observationDateYear']==year) & (one_country_table['observationDateMonth']==month)]
+                            
+                            if len(case_in_month) != 0:        
+                                number_cases = case_in_month.sumCases.sum()
+                                
+                                final_data = final_data.append( pd.DataFrame({'region':country,
+                                     'observationDateYear': year,
+                                     'observationDateMonth': month,
+                                     'disease': disease,
+                                     'sumCases': number_cases},index=[len(final_data)]), ignore_index = True)
+
+    #print final_data
+    final_data = final_data.dropna()
+    final_data = final_data.reset_index()
+    train_y = final_data[:,5:]
+    print train_y
+
+    
+    
