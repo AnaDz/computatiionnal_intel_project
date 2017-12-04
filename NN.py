2ae5ef88d7479727bda68b1f0437d7e9b69e2c16
anaDz <anais.donzeau@hotmail.fr>
2017-12-04 02:37:51 -0500

    version finale

--- NN.py
+++ NN.py
@@ -8,13 +8,13 @@ from sklearn.preprocessing import StandardScaler
 flags = tf.flags
 flags.DEFINE_integer('n_in', 4, 'input size')
 flags.DEFINE_integer('n_out', 1, 'output size')
-# has to be between n_in and n_out
 flags.DEFINE_integer('n_hidden', 5, 'hidden layer size')
 flags.DEFINE_float('learning_rate', 0.1, 'learning rate')
+flags.DEFINE_integer('batch_size', 40, 'mini-batch size')
 FLAGS = flags.FLAGS
 
 
-def construct_MLP():    
+def construct_NN():    
     W_hid = tf.Variable(uniform( low=-0.01 , high=0.01, size=(FLAGS.n_in,FLAGS.n_hidden)).astype('float32'), 
                         name='W_h')
     b_hid = tf.Variable( np.zeros( [FLAGS.n_hidden], dtype='float32'),name='b_h')
@@ -45,10 +45,14 @@ def train_network(epochs):
 
     train_costs = np.zeros(epochs, dtype='float32');
     train_accuracies = np.zeros(epochs, dtype='float32');
+
     ### TRAINING BEGIN ###
     print "Epoch  Cost "
-    for i in xrange(epochs):
-        train_cost, _ = sess.run([cost, train_op], feed_dict={input_x: train_X, label_y: train_y})
+    for i in range(epochs):
+
+        for start, end in zip(range(0, train_X.shape[0], FLAGS.batch_size), range(FLAGS.batch_size, train_X.shape[0], FLAGS.batch_size)):
+             train_cost, _ = sess.run([cost, train_op], feed_dict={input_x: train_X[start:end], label_y: train_y[start:end]})
+
         train_costs[i] = train_cost
         if i % 200 == 0:
             print "%05d  %5.3f" % (i,train_cost);
@@ -78,13 +82,10 @@ if __name__ == '__main__':
 
 #    print scY.inverse_transform(train_y.reshape(-1,1))
 
-    input_x, label_y,cost, train_op, y_pred = construct_MLP()
-    train_costs, y_pred, sess = train_network(2000)
+    input_x, label_y,cost, train_op, y_pred = construct_NN()
+    train_costs, y_pred, sess = train_network(5000)
 
-    #print y_pred
-
-    outputs = sess.run( y_pred, feed_dict={input_x: train_X, label_y: train_y});
+    outputs = sess.run( y_pred, feed_dict={input_x: valid_X, label_y: valid_y});
 
     print scY.inverse_transform(outputs[0:5].reshape(-1,1))
     print scY.inverse_transform(train_y[0:5].reshape(-1,1))
-    
--- prep_data.py
+++ prep_data.py
@@ -9,43 +9,30 @@ import os
 import datetime
 
 if __name__ == '__main__':
-    if (not os.path.exists('good_data.csv')):
-        
-        data = pd.read_csv('Outbreak_240817.csv')
-        data = data.drop(columns=['source', 'latitude', 'longitude', 'country', 'admin1', 'localityName', 'localityQuality', 'reportingDate', 'status', 'serotypes', 'speciesDescription', 'sumAtRisk', 'sumDeaths', 'sumDestroyed', 'sumSlaughtered', 'humansGenderDesc', 'humansAge', 'humansAffected', 'humansDeaths'])
-        
-        data = data.dropna()
-        data = data.reset_index()
-        data = data.drop(columns=['index'])
 
-        first_date = datetime.datetime.strptime(data.loc[0,'observationDate'], "%d/%m/%Y")
-        data_process = pd.DataFrame({'region':data.loc[0,'region'],
-                                     'observationDateYear': first_date.year,
-                                     'observationDateMonth': first_date.month,
-                                     'disease': data.loc[0,'disease'],
-                                     'sumCases': data.loc[0,'sumCases']},index=[0])
-        
-#        print data.head()
-        for i in range(1,len(data.observationDate)):
-            date = datetime.datetime.strptime(data.loc[i,'observationDate'], "%d/%m/%Y")
-            data_process = data_process.append(pd.DataFrame({'region':data.loc[i,'region'],
-                                     'observationDateYear': date.year,
-                                     'observationDateMonth': date.month,
-                                     'disease': data.loc[i,'disease'],
-                                              'sumCases': data.loc[i,'sumCases']},index=[i]), ignore_index = True)
-            #if (isinstance(date, str)):
-                   #data.loc[i,'observationDate'] = datetime.datetime.strptime(date, "%d/%m/%Y")
-       # print(data.observationDate[1])
-        data_process.to_csv('good_data.csv')
-
-        #processing data : creation of a new panda table with month and year instead of one date
-        
-        print data_process
-
-    else:
-        data_process = pd.read_csv('good_data.csv')
-
+    data = pd.read_csv('Outbreak_240817.csv')
+    data = data.drop(columns=['source', 'latitude', 'longitude', 'country', 'admin1', 'localityName', 'localityQuality', 'reportingDate', 'status', 'serotypes', 'speciesDescription', 'sumAtRisk', 'sumDeaths', 'sumDestroyed', 'sumSlaughtered', 'humansGenderDesc', 'humansAge', 'humansAffected', 'humansDeaths'])
     
+    data = data.dropna()
+    data = data.reset_index()
+    data = data.drop(columns=['index'])
+
+    first_date = datetime.datetime.strptime(data.loc[0,'observationDate'], "%d/%m/%Y")
+    data_process = pd.DataFrame({'region':data.loc[0,'region'],
+                                 'observationDateYear': first_date.year,
+                                 'observationDateMonth': first_date.month,
+                                 'disease': data.loc[0,'disease'],
+                                 'sumCases': data.loc[0,'sumCases']},index=[0])
+    
+    for i in range(1,len(data.observationDate)):
+        date = datetime.datetime.strptime(data.loc[i,'observationDate'], "%d/%m/%Y")
+        data_process = data_process.append(pd.DataFrame({'region':data.loc[i,'region'],
+                                                             'observationDateYear': date.year,
+                                                             'observationDateMonth': date.month,
+                                                             'disease': data.loc[i,'disease'],
+                                                             'sumCases': data.loc[i,'sumCases']},index=[i]), ignore_index = True)
+        
+
     dict_country = dict()
     dict_disease = dict()
     #most_common_diseases = data.disease.index
@@ -60,9 +47,9 @@ if __name__ == '__main__':
     for country in all_countries:
         dict_country[country] = index
         index = index + 1
-    
-    final_data =  pd.DataFrame()
         
+    final_data =  pd.DataFrame()
+    
     for disease in all_diseases:
         one_disease_table = data_process.loc[data_process['disease'] == disease]
         for country in all_countries:
@@ -81,10 +68,10 @@ if __name__ == '__main__':
                                 number_cases = case_in_month.sumCases.sum()
                                 
                                 final_data = final_data.append( pd.DataFrame({'region':dict_country[country],
-                                     'observationDateYear': year,
-                                     'observationDateMonth': month,
-                                     'disease': dict_disease[disease],
-                                     'sumCases': number_cases},index=[len(final_data)]), ignore_index = True)
+                                                                              'observationDateYear': year,
+                                                                              'observationDateMonth': month,
+                                                                              'disease': dict_disease[disease],
+                                                                              'sumCases': number_cases},index=[len(final_data)]), ignore_index = True)
 
 
 
